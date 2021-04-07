484ee767ad392c8f0f41ee4daf5e76ba92ad53b7
mamartinod <ma.martinod@free.fr>
2021-04-07 15:12:03 +1000

    Add error on ephemerides. Make the code partially PEP8 compliant

--- binary_tools.py
+++ binary_tools.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-### plot modules
+# plot modules
 import matplotlib.pyplot as plt
 
-### Ephemerides libraries
+# Ephemerides libraries
 from astroplan import Observer, FixedTarget
 import datetime
 from astropy.time import Time
@@ -12,11 +12,11 @@ import sympy as sp
 plt.ion()
 
 
-def get_Parallactic_Angle(starname, year,month,day,hours,minutes,seconds, display = False):
+def get_Parallactic_Angle(starname, year, month, day, hours, minutes, seconds, display=False):
     """
     Get the parallactic angle calculated by Astroplan, using celestial coordinates and angle hour.
     The formula is taken from https://en.wikipedia.org/wiki/Parallactic_angle
-    
+
     :param starname: Name of the star (Common name or from any catalog known by SIMBAD)
     :type starname: string
     :param year: year of the observation, in HST time zone
@@ -37,37 +37,38 @@ def get_Parallactic_Angle(starname, year,month,day,hours,minutes,seconds, displa
     :rtype: tuple
 
     """
-    ## Theoretical Parallactic angle
-    ## Select site of observation
+    # Theoretical Parallactic angle
+    # Select site of observation
     subaru = Observer.at_site("Subaru", timezone="US/Hawaii")
 
-    ## Select date
-    date_time = datetime.datetime(year,month,day,hours,minutes,seconds)
-    utc_time = subaru.datetime_to_astropy_time(date_time) # convert to UTC
+    # Select date
+    date_time = datetime.datetime(year, month, day, hours, minutes, seconds)
+    utc_time = subaru.datetime_to_astropy_time(date_time)  # convert to UTC
 
-    ## Select Target
-    star=FixedTarget.from_name(starname)
+    # Select Target
+    star = FixedTarget.from_name(starname)
 
-    ## Compute parallactic angle = angle between Altaz orientation and North
+    # Compute parallactic angle = angle between Altaz orientation and North
     para_angle = subaru.parallactic_angle(utc_time, star).deg
     if display is True:
         print('Parallactic angle :'+str(para_angle)+' degrees')
 
-    ## Chuck's PAD 
-    chuck_pad = para_angle +180 - 39
+    # Chuck's PAD
+    chuck_pad = para_angle + 180 - 39
     if display is True:
         print('Parallactic angle for Chuck (PAD) :'+str(chuck_pad)+' degrees')
 
     return para_angle, utc_time
 
-def get_Parallactic_Angle_Subaru(Az, El, lat=None, display = False):
+
+def get_Parallactic_Angle_Subaru(Az, El, lat=None, display=False):
     """
     Get the parallactic angle as coded in AO-188 and from the telemetry saved by it.
     Note, the parallactic given here is different from the one given by ``get_Parallactic_Angle''.
     It is not confirmed yet but here are two possible reasons: 
         * the numerical errors (Astroplan formula uses lots of trigonometry)
         * the levation calculated in AO-188 is not the exact same one given by Astroplan for the exact same time of observation
-    
+
     :param Az: Azimuth of the object
     :type Az: float
     :param El: Elevation of the object
@@ -80,13 +81,13 @@ def get_Parallactic_Angle_Subaru(Az, El, lat=None, display = False):
     :rtype: tuple
 
     """
-    ## Formula from Kudo-san to compute the PAD on chuck from telescope AzEl info - from AO188 telemetry
+    # Formula from Kudo-san to compute the PAD on chuck from telescope AzEl info - from AO188 telemetry
     if lat is None:
         lat = 19.823806 * np.pi / 180
     else:
         lat = lat * np.pi / 180
-    Az  = (Az) * np.pi / 180
-    El  = El * np.pi / 180
+    Az = (Az) * np.pi / 180
+    El = El * np.pi / 180
 
     deg = np.sin(El) * np.cos(Az) + np.cos(El)*np.sin(lat)/np.cos(lat)
     para_angle_Subaru = np.arctan2(np.sin(Az), deg) * 180 / np.pi
@@ -94,10 +95,11 @@ def get_Parallactic_Angle_Subaru(Az, El, lat=None, display = False):
     # if display is True:
     #     print('Parallactic angle for Subaru :'+str(para_angle_Subaru)+' degrees')
 
-    ## Chuck's PAD 
-    chuck_pad = para_angle_Subaru +180 - 39
+    # Chuck's PAD
+    chuck_pad = para_angle_Subaru + 180 - 39
     if display is True:
-        print('Subaru parallactic angle for Chuck (PAD) :'+str(chuck_pad)+' degrees')
+        print('Subaru parallactic angle for Chuck (PAD) :' +
+              str(chuck_pad)+' degrees')
 
     return chuck_pad, para_angle_Subaru
 
@@ -105,7 +107,7 @@ def get_Parallactic_Angle_Subaru(Az, El, lat=None, display = False):
 def binary_orbit_calc(t, smaj, ecc, inc, small_om, big_om):
     """
     Calculate the orbit of a companion around the host in a binary system.
-    
+
     :param smaj: length of the semi-major axis, in arcsecond
     :type smaj: float
     :param ecc: inclination of the orbit
@@ -129,29 +131,33 @@ def binary_orbit_calc(t, smaj, ecc, inc, small_om, big_om):
     """
 
     # Conversions
-    inc         = np.deg2rad(inc)
-    small_om    = np.deg2rad(small_om)
-    big_om      = np.deg2rad(big_om)
+    inc = np.deg2rad(inc)
+    small_om = np.deg2rad(small_om)
+    big_om = np.deg2rad(big_om)
 
     # Defining intermediate variables
-    exq         = np.sqrt(1.-ecc*ecc)
-    uu1         = np.cos(small_om)*np.cos(big_om)-np.sin(small_om)*np.sin(big_om)*np.cos(inc)
-    uu2         = -1.*np.sin(small_om)*np.cos(big_om)-np.cos(small_om)*np.sin(big_om)*np.cos(inc)
-    uu3         = np.cos(small_om)*np.sin(big_om)+np.sin(small_om)*np.cos(big_om)*np.cos(inc)
-    uu4         = -1.*np.sin(small_om)*np.sin(big_om)+np.cos(small_om)*np.cos(big_om)*np.cos(inc)
-
+    exq = np.sqrt(1.-ecc*ecc)
+    uu1 = np.cos(small_om)*np.cos(big_om) - \
+        np.sin(small_om)*np.sin(big_om)*np.cos(inc)
+    uu2 = -1.*np.sin(small_om)*np.cos(big_om) - \
+        np.cos(small_om)*np.sin(big_om)*np.cos(inc)
+    uu3 = np.cos(small_om)*np.sin(big_om) + \
+        np.sin(small_om)*np.cos(big_om)*np.cos(inc)
+    uu4 = -1.*np.sin(small_om)*np.sin(big_om) + \
+        np.cos(small_om)*np.cos(big_om)*np.cos(inc)
 
     # Computing positions along the orbit
     u = 4.*np.pi*t
-    x_pos    = (uu1*(np.cos(u)-ecc)*smaj+uu2*smaj*exq*np.sin(u))
-    y_pos    = (uu3*(np.cos(u)-ecc)*smaj+uu4*smaj*exq*np.sin(u))
-    
+    x_pos = (uu1*(np.cos(u)-ecc)*smaj+uu2*smaj*exq*np.sin(u))
+    y_pos = (uu3*(np.cos(u)-ecc)*smaj+uu4*smaj*exq*np.sin(u))
+
     return x_pos, y_pos
-    
-def binary_orbit(n_points, t0, T, smaj, ecc, inc, small_om, big_om, display = False):
+
+
+def binary_orbit(n_points, t0, T, smaj, ecc, inc, small_om, big_om, display=False):
     """
     Plot the orbit of a binary system
-    
+
     :param n_points: Number of points to plot
     :type n_points: int
     :param smaj: semi-major axis, in arcsecond
@@ -171,37 +177,39 @@ def binary_orbit(n_points, t0, T, smaj, ecc, inc, small_om, big_om, display = Fa
 
     """
 
-    x_pos       = np.zeros(n_points)
-    y_pos       = np.zeros(n_points)
-    epoch       = np.zeros(n_points)
-    
-    nn          = 2.*np.pi/T
+    x_pos = np.zeros(n_points)
+    y_pos = np.zeros(n_points)
+    epoch = np.zeros(n_points)
+
+    nn = 2.*np.pi/T
 
     for i in range(n_points):
-        t                   = i/(n_points-1)
-        u                   = 4.*np.pi*t
-        epoch[i]            = t0+(u-ecc*np.sin(u))/nn
-        x_pos[i], y_pos[i]  = binary_orbit_calc(t, smaj, ecc, inc, small_om, big_om)
+        t = i/(n_points-1)
+        u = 4.*np.pi*t
+        epoch[i] = t0+(u-ecc*np.sin(u))/nn
+        x_pos[i], y_pos[i] = binary_orbit_calc(
+            t, smaj, ecc, inc, small_om, big_om)
 
-        
     # Plot orbit
     if display is True:
         plt.figure()
         plt.plot(y_pos, x_pos)
         plt.ylabel('Declination (arcsec)')
         plt.xlabel("Right ascension (arcsec)")
-        plt.xlim([0.06,-0.06])
-        plt.ylim([-0.06,0.06])
-        plt.scatter(0,0,c='black',marker='+')
+        plt.xlim([0.06, -0.06])
+        plt.ylim([-0.06, 0.06])
+        plt.scatter(0, 0, c='black', marker='+')
         plt.axis('equal')
-        
+
     return epoch, x_pos, y_pos
 
 
-def binary_ephem(current_date, t0, T, smaj, ecc, inc, small_om, big_om, display=False):
+def binary_ephem(current_date, t0, T, smaj, ecc, inc, small_om, big_om, t0_err,
+                 T_err, smaj_err, ecc_err, inc_err, small_om_err, big_om_err,
+                 nsamp, display=False):
     """
     Gives the separation and PA of the companion at the date of observation.
-    
+
     :param current_date: Date of observation, in decimal year
     :type current_date: float
     :param t0: Periastron date, in decimal year
@@ -224,43 +232,69 @@ def binary_ephem(current_date, t0, T, smaj, ecc, inc, small_om, big_om, display=
     :rtype: tuple
 
     """
-    
-    date_sub = np.mod((current_date-t0), T) # Time spent after passing periastron
-    # Solve equation to find the actual time locating the position of the companion
-    x = sp.symbols('x', real=True)
-    eq = sp.Eq(4*sp.pi*x-ecc*sp.sin(4*sp.pi*x), 2*sp.pi/T*date_sub)
-    t = sp.nsolve(eq, x, 0)
-    t = float(t)
-    
-    # Load orbit info
-    x_date, y_date = binary_orbit_calc(t, smaj, ecc, inc, small_om, big_om)
 
-    # Compute Sep. and Position Angle (PA)
-    sep         = np.sqrt(x_date**2+y_date**2)
-    pa          = np.rad2deg(np.arctan2(y_date,x_date))
+    sep, pa = [], []
+    if nsamp <= 0:
+        nsamp = 1
 
-    if pa < 0.:
-        pa += 360.
+    for k in range(nsamp):
+        if nsamp <= 1 or k == 0:
+            t0_samp = t0
+            smaj_samp = smaj
+            ecc_samp = ecc
+            inc_samp = inc
+            small_om_samp = small_om
+            big_om_samp = big_om
+            T_samp = T
+        else:
+            t0_samp = np.random.normal(t0, t0_err)
+            smaj_samp = np.random.normal(smaj, smaj_err)
+            ecc_samp = np.random.normal(ecc, ecc_err)
+            inc_samp = np.random.normal(inc, inc_err)
+            small_om_samp = np.random.normal(small_om, small_om_err)
+            big_om_samp = np.random.normal(big_om, big_om_err)
+            T_samp = np.random.normal(T, T_err)
+        # Time spent after passing periastron
+        date_sub = np.mod((current_date-t0_samp), T_samp)
+        # Solve equation to find the actual time locating
+        # the position of the companion
+        x = sp.symbols('x', real=True)
+        eq = sp.Eq(4*sp.pi*x-ecc_samp*sp.sin(4*sp.pi*x),
+                   2*sp.pi/T_samp*date_sub)
+        t = sp.nsolve(eq, x, 0)
+        t = float(t)
 
+        # Load orbit info
+        x_date, y_date = binary_orbit_calc(t, smaj_samp, ecc_samp, inc_samp,
+                                           small_om_samp, big_om_samp)
 
-    print('Binary separation :'+str(sep*1000)+' mas')
-    print('Binary Position Angle :'+str(pa)+' degrees')
+        # Compute Sep. and Position Angle (PA)
+        sep.append(np.sqrt(x_date**2+y_date**2))
+        pa.append(np.rad2deg(np.arctan2(y_date, x_date)) % 360)
 
-    x_pos, y_pos = binary_orbit(1000, t0, T, smaj, ecc, inc, small_om, big_om, display = False)[1:]
-    
-    if display is True :
+    sep = np.array(sep) * 1000
+    pa = np.array(pa)
+
+    print('Binary separation: %.5f +/- %.5f mas' % (sep[0], sep.std()))
+    print('Binary Position Angle: %.5f +/- %.5f degrees' %
+          (pa[0], pa.std()))
+
+    if display is True:
+        x_pos, y_pos = binary_orbit(
+            1000, t0, T, smaj, ecc, inc, small_om, big_om, display=False)[1:]
+
         plt.figure()
         plt.plot(y_pos, x_pos)
-        plt.plot(y_date,x_date,c='red',marker='+')
+        plt.plot(y_date, x_date, c='red', marker='+')
         plt.ylabel('Declination (arcsec)')
         plt.xlabel("Right ascension (arcsec)")
-        plt.xlim([0.06,-0.06])
-        plt.ylim([-0.06,0.06])
-        plt.scatter(0,0,c='black',marker='+')
+        plt.xlim([0.06, -0.06])
+        plt.ylim([-0.06, 0.06])
+        plt.scatter(0, 0, c='black', marker='+')
         plt.axis('equal')
-        
+
     return sep, pa
-    
+
 
 def get_Orbit_Param(Target):
     """
@@ -268,128 +302,130 @@ def get_Orbit_Param(Target):
 
     """
 
-
     if Target == 'Capella':
+        # Source: https://ui.adsabs.harvard.edu/abs/2015ApJ...807...26T/abstract
         # Time of Periastron [years]
-        t0         = 1990.6984257357974
+        t0 = 1990.6989041095892
+        t0_err = 0.007123287671674916
         # Semi-major axis [asec]
-        smaj       = 0.056442
+        smaj = 0.056442
+        smaj_err = 0.023e-3
         # Eccentricity
-        ecc        = 0.00089
+        ecc = 0.00089
+        ecc_err = 0.00011
         # Inclination [degrees]
-        inc        = 137.156
+        inc = 137.156
+        inc_err = 0.046
         # Argument of periastron
-        small_om   = 342.6
+        small_om = 342.6
+        small_om_err = 9.
         # Position angle of ascending node
-        big_om     = 40.522
+        big_om = 40.522
+        big_om_err = 0.039
         # Period [years]
-        T          = 0.28479474332648874   
+        T = 0.28479474332648874
+        T_err = 4.3805612594113626e-07
 
-    elif  Target == 'betaHer':
+    elif Target == 'betaHer':
         # Time of Periastron [years]
-        t0 = Time('2415500.4',format='jd')
-        t0         = t0.decimalyear
+        t0 = Time('2415500.4', format='jd')
+        t0 = t0.decimalyear
+        t0_err = 0.
         # Semi-major axis [asec]
-        smaj       = 0.01137
+        smaj = 0.01137
+        smaj_err = 0.
         # Eccentricity
-        ecc        = 0.55
+        ecc = 0.55
+        ecc_err = 0.
         # Inclination [degrees]
-        inc        = 53.8
+        inc = 53.8
+        inc_err = 0.
         # Argument of periastron
-        small_om   = 24.6
+        small_om = 24.6
+        small_om_err = 0.
         # Position angle of ascending node
-        big_om     = 341.9
+        big_om = 341.9
+        big_om_err = 0.
         # Period [years]
         T = 410.6/365.25
+        T_err = 0.
 
     elif Target == 'delSge':
         # Time of Periastron [years]
-        t0         = 1979.93
+        t0 = 1979.93
+        t0_err = 0.
         # Semi-major axis [asec]
-        smaj       = 0.051
+        smaj = 0.051
+        smaj_err = 0.
         # Eccentricity
-        ecc        = 0.44
+        ecc = 0.44
+        ecc_err = 0.
         # Inclination [degrees]
-        inc        = 140
+        inc = 140
+        inc_err = 0.
         # Argument of periastron
-        small_om   = 257.7
+        small_om = 257.7
+        small_om_err = 0.
         # Position angle of ascending node
-        big_om     = 170.2
+        big_om = 170.2
+        big_om_err = 0.
         # Period [years]
-        T          = 10.11
+        T = 10.11
+        T_err = 0.
 
     else:
         print('Target not listed...')
 
-
-
-    return t0, smaj, ecc, inc, small_om, big_om, T
-
-
-
+    return t0, smaj, ecc, inc, small_om, big_om, T,\
+        t0_err, smaj_err, ecc_err, inc_err, small_om_err, big_om_err, T_err
 
 
 if __name__ == "__main__":
 
-    # # Time of Periastron [years]
-    # t0         = 1990.6984257357974
-    # # Semi-major axis [asec]
-    # smaj       = 0.056442
-    # # Eccentricity
-    # ecc        = 0.00089
-    # # Inclination [degrees]
-    # inc        = 137.156
-    # # Argument of periastron
-    # small_om   = 342.6
-    # # Position angle of ascending node
-    # big_om     = 40.522
-    # # Period [years]
-    # T          = 0.28479474332648874
+    # Load Orbits params
+    t0, smaj, ecc, inc, small_om, big_om, T,\
+        t0_err, smaj_err, ecc_err, inc_err, small_om_err, big_om_err, T_err =\
+        get_Orbit_Param('Capella')
 
-
-    ## Load Orbits params
-    t0, smaj, ecc, inc, small_om, big_om, T = get_Orbit_Param('Capella')
-
-    ## Orbit    
+    # Orbit
     # epoch, x_pos, y_pos = binary_orbit(1000, t0, T, smaj, ecc, inc, small_om, big_om, display = False)
 
-
-    date = Time('2021-03-19T00:00:00', format='isot')
+    date = Time('2020-09-17T04:50:18', format='isot')
     subaru = Observer.at_site("Subaru", timezone="US/Hawaii")
 
-    ## Select date
-    utc_time = subaru.datetime_to_astropy_time(date.datetime) # convert to UTC
-    current_date = 2021.2123
+    # Select date
+    utc_time = subaru.datetime_to_astropy_time(date.datetime)  # convert to UTC
+    # utc_time = Time(datetime.datetime.now())
+    current_date = utc_time.decimalyear
 
-    ## Compute / display binary ephem
-    sep, pa = binary_ephem(current_date, t0, T, smaj, ecc, inc, small_om, big_om, display=True)
-    
+    # Compute / display binary ephem
+    sep, pa = binary_ephem(current_date, t0, T, smaj, ecc, inc, small_om,
+                           big_om, t0_err, T_err, smaj_err, ecc_err, inc_err,
+                           small_om_err, big_om_err, nsamp=1000, display=True)
 
-    ## Get the Parallactic Angle from astroplan library
-    PA = get_Parallactic_Angle('Capella', 2020,9,16,4,48,32, display=True)
-    ## Get the Parallactic Angle from Telescope position # WARNING : IF Az comes from AO188 telemetry, need to subtract 180 degrees before using get_Parallactic_Angle_Subaru()
-    Az = 17.679
-    El = 62.0691
+    # ## Get the Parallactic Angle from astroplan library
+    # PA = get_Parallactic_Angle('Capella', 2020,9,16,4,48,32, display=True)
+    # ## Get the Parallactic Angle from Telescope position # WARNING : IF Az comes from AO188 telemetry, need to subtract 180 degrees before using get_Parallactic_Angle_Subaru()
+    # Az = 17.679
+    # El = 62.0691
 
-    PAD, subaru_q = get_Parallactic_Angle_Subaru(Az, El, display = True)
+    # PAD, subaru_q = get_Parallactic_Angle_Subaru(Az, El, display = True)
 
+    # date = Time('2020-09-16T14:48:32', format='isot')
+    # print('Seb', get_Parallactic_Angle('Capella', date.datetime.year, date.datetime.month, date.datetime.day, \
+    #                               date.datetime.hour-10, date.datetime.minute, date.datetime.second)[0])
 
-    
-    date = Time('2020-09-16T14:48:32', format='isot')    
-    print('Seb', get_Parallactic_Angle('Capella', date.datetime.year, date.datetime.month, date.datetime.day, \
-                                  date.datetime.hour-10, date.datetime.minute, date.datetime.second)[0])
-        
-    subaru = Observer.at_site("Subaru", timezone="US/Hawaii")
-    star=FixedTarget.from_name("Capella")
-    print(subaru.altaz(date, star).alt.deg, subaru.altaz(date, star).az.deg)
-    print('Astroplan', subaru.parallactic_angle(date, star).deg)
-    print('Subaru', get_Parallactic_Angle_Subaru(subaru.altaz(date, star).az.deg, subaru.altaz(date, star).alt.deg, display = False, lat=subaru.location.lat.deg)[1])
-    
-    LST = date.sidereal_time('mean', longitude=subaru.location.lon)
-    H = (LST - star.ra).radian
-    latitude = subaru.location.lat.radian
-    declinaison = star.dec.radian
-    sin_parallactic_angle = np.sin(H) * np.cos(latitude)
-    cos_parallactic_angle = np.cos(declinaison) * np.sin(latitude) - np.sin(declinaison) * np.cos(latitude) * np.cos(H)
-    parallactic_angle = np.arctan2(sin_parallactic_angle, cos_parallactic_angle) * 180/np.pi
-    print('A la main', parallactic_angle)    
+    # subaru = Observer.at_site("Subaru", timezone="US/Hawaii")
+    # star=FixedTarget.from_name("Capella")
+    # print(subaru.altaz(date, star).alt.deg, subaru.altaz(date, star).az.deg)
+    # print('Astroplan', subaru.parallactic_angle(date, star).deg)
+    # print('Subaru', get_Parallactic_Angle_Subaru(subaru.altaz(date, star).az.deg, subaru.altaz(date, star).alt.deg, display = False, lat=subaru.location.lat.deg)[1])
+
+    # LST = date.sidereal_time('mean', longitude=subaru.location.lon)
+    # H = (LST - star.ra).radian
+    # latitude = subaru.location.lat.radian
+    # declinaison = star.dec.radian
+    # sin_parallactic_angle = np.sin(H) * np.cos(latitude)
+    # cos_parallactic_angle = np.cos(declinaison) * np.sin(latitude) - np.sin(declinaison) * np.cos(latitude) * np.cos(H)
+    # parallactic_angle = np.arctan2(sin_parallactic_angle, cos_parallactic_angle) * 180/np.pi
+    # print('A la main', parallactic_angle)
